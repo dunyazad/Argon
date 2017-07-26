4f8c0d3f8724fca1eee3392149df0d60bd11190f
Mickey <dunyazad@hanmail.net>
2017-07-26 17:49:50 +0900

    Rename ANVAO => ANGraphicsBufferArray, ANVBO => ANGraphicsBuffer

---
+++ LibArgon/Component/ANGraphicsBuffer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Text;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Input;
+using QuickFont;
+using QuickFont.Configuration;
+
+namespace ArtificialNature
+{
+    public abstract class ANGraphicsBufferBase : ANComponent
+    {
+        public abstract void BufferData();
+    }
+
+    public class ANGraphicsBuffer<T> : ANGraphicsBufferBase where T : struct
+    {
+        public ANGraphicsBufferArray VAO { get; set; }
+        public int AttributeID { get; private set; }
+
+        protected int vbo;
+
+        public List<T> Datas { get; set; }
+
+        int dataUnitSize;
+
+        public override void OnInitialize()
+        {
+            Datas = new List<T>();
+
+            dataUnitSize = System.Runtime.InteropServices.Marshal.SizeOf(default(T));
+
+            GL.GenBuffers(1, out vbo);
+            AttributeID = GL.GetAttribLocation(VAO.Shader.Program, Name);
+        }
+
+        public override void OnRender()
+        {
+        }
+
+        public override void OnTerminate()
+        {
+            GL.DeleteBuffer(vbo);
+        }
+
+        public override void OnUpdate(double dt)
+        {
+        }
+
+        public void Bind()
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+        }
+
+        public void Unbind()
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+        }
+
+        public void Enable()
+        {
+            GL.EnableVertexAttribArray(AttributeID);
+        }
+
+        public void Disable()
+        {
+            GL.DisableVertexAttribArray(AttributeID);
+        }
+
+        public void AddData(T data)
+        {
+            Datas.Add(data);
+        }
+
+        public void AddData(T[] datas)
+        {
+            this.Datas.AddRange(datas);
+        }
+
+        public override void BufferData()
+        {
+            Bind();
+            GL.BufferData<T>(BufferTarget.ArrayBuffer, (IntPtr)(Datas.Count * dataUnitSize), Datas.ToArray(), BufferUsageHint.StaticDraw);
+            GL.VertexAttribPointer(AttributeID, dataUnitSize / sizeof(float), VertexAttribPointerType.Float, false, 0, 0);
+            Unbind();
+        }
+    }
+}
---
+++ LibArgon/Component/ANGraphicsBufferArray.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Text;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Input;
+using QuickFont;
+using QuickFont.Configuration;
+
+namespace ArtificialNature
+{
+    public class ANGraphicsBufferArray : ANComponent
+    {
+        public ANShader Shader { get; set; }
+
+        protected int vao;
+
+        Dictionary<string, ANGraphicsBufferBase> buffers = new Dictionary<string, ANGraphicsBufferBase>();
+
+
+        public override void OnInitialize()
+        {
+            GL.GenVertexArrays(1, out vao);
+
+            foreach (var kvp in buffers)
+            {
+                kvp.Value.OnInitialize();
+            }
+        }
+
+        public override void OnUpdate(double dt)
+        {
+        }
+
+        public override void OnRender()
+        {
+        }
+
+        public override void OnTerminate()
+        {
+            foreach (var kvp in buffers)
+            {
+                kvp.Value.OnTerminate();
+            }
+
+            GL.DeleteVertexArray(vao);
+        }
+
+        public void Bind()
+        {
+            GL.BindVertexArray(vao);
+        }
+
+        public void Unbind()
+        {
+            GL.BindVertexArray(0);
+        }
+
+        public ANGraphicsBuffer<T> CreateVBO<T>(string name) where T : struct
+        {
+            if (buffers.ContainsKey(name))
+            {
+                return buffers[name] as ANGraphicsBuffer<T>;
+            }
+            else
+            {
+                var vbo = new ANGraphicsBuffer<T>() { Name = name, VAO = this };
+                buffers.Add(name, vbo);
+                return vbo;
+            }
+        }
+
+        public void BufferData()
+        {
+            Bind();
+            foreach (var kvp in buffers)
+            {
+                kvp.Value.BufferData();
+            }
+            Unbind();
+        }
+    }
+}
